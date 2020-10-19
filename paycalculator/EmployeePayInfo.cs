6f9407706f0718395b36f6f145b99b1278adf295
Osman <Aala.Osman@tpr.gov.uk>
2020-10-19 16:05:07 +0100

    Add project files.

---
+++ paycalculator/EmployeePayInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+
+namespace paycalculator
+{
+    class EmployeePayInfo
+    {
+        double annualsalary;  
+        double annualbonus;
+     
+        public double AnnualSalary { get { return annualsalary;} }
+        public EmployeePayInfo(string name, double annualsalary, double annualbonus) 
+        {
+            this.annualsalary = annualsalary;
+            this.annualbonus = annualbonus;
+        }
+        public void calculateTotalAnnualandBounsPay() 
+        {
+            double annual = annualsalary;
+            double bonus = annualbonus;
+            double total = annual + bonus;  
+        }
+    }
+}
---
+++ paycalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+
+namespace paycalculator
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            EmployeePayInfo eo = new EmployeePayInfo("Aala", 22.000, 4.000);
+            eo.calculateTotalAnnualandBounsPay();
+            Console.WriteLine(eo.AnnualSalary);
+        }
+    }
+}
